706b5c440217849abd171ef8af40602c0cb67e80
Andrew G <agoodman9@mail.valenciacollege.edu>
2023-05-15 12:36:47 -0400

    Added Javadoc Comments Per Request

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Fibonacci {
+    /**
+     * Calculate the nth term in the Fibonacci sequence.
+     * @param n the index of the term to calculate (non-negative integer)
+     * @return the nth term in the Fibonacci sequence
+     */
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
@@ -8,6 +13,12 @@ public class Fibonacci {
             return fibonacci(n - 1)+fibonacci(n - 2);
         }
     }
+    /**
+     * Prompts the user to enter a integer and calculates the corresponding term in the Fibonacci sequence.
+     * Displays the result in the console.
+     *
+     * @param args command line argument is not used
+     */
     public static void main(String[] args) {
         System.out.println("Enter a number to find the term in the Fibonacci sequence: ");
         Scanner scan = new Scanner(System.in);
