696d84a0bf6851b88c81031284e21ad3e5de1d2c
Andrew G <agoodman9@mail.valenciacollege.edu>
2023-06-20 11:34:30 -0400

    Javadoc comments added.

--- BasicCalculator/src/main/java/com/example/Calculator.java
+++ BasicCalculator/src/main/java/com/example/Calculator.java
@@ -2,19 +2,47 @@ package main.java.com.example;
 
 import java.util.Scanner;
 
+/**
+ * This calculator class provides basic mathematics functions.
+ */
 public class Calculator {
+    /**
+     * Adds two numbers
+     * @param a The First Number
+     * @param b The Second Number
+     * @return The sum of the two numbers.
+     */
     public int add(int a, int b) {
         return a + b;
     }
 
+    /**
+     * Subtrackts two numbers.
+     * @param a The number that is subtracted from.
+     * @param b The number to subtract.
+     * @return The differences tween the two numbers.
+     */
     public int subtract(int a, int b) {
         return a - b;
     }
 
+    /**
+     * Multiplies two numbers.
+     * @param a The first number.
+     * @param b The second number.
+     * @return The product of the two numbers.
+     */
     public int multiply(int a, int b) {
         return a * b;
     }
 
+    /**
+     * Divides one number by another number.
+     * @param a The Dividend.
+     * @param b The divisor.
+     * @return The divison result as a double.
+     * @throws IllegalArgumentException if the divisor is a zero.
+     */
     public double divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Division By Zero Is Not Allowed.");
@@ -22,10 +50,22 @@ public class Calculator {
         return (double) a / b;
     }
 
+    /**
+     * Calculates the square of a number
+     * @param a The number to be squared
+     * @return The square of the number.
+     */
     public int square(int a) {
         return a * a;
     }
 
+    /**
+     * Calculates the power of a number raised to an exponential number.
+     * @param base The base number
+     * @param exponent The exponent
+     * @return The result of raising the base to the power of the exponent
+     * @throws IllegalArgumentException If the exponent is a negative number.
+     */
     public int power(int base, int exponent) {
         if (exponent < 0) {
             throw new IllegalArgumentException("Exponent Must Be A Non-Negative Number.");
@@ -37,6 +77,10 @@ public class Calculator {
         return result;
     }
 
+    /**
+     * The main method that allows the calculator to be used interactively
+     * @param args Command-line arguments (not used in this program)
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
